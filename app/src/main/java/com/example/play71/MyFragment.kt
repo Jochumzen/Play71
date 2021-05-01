7b26343d38d947d12a491544bdf8f5061c61f1d4
Jens <jens.c.norell@gmail.com>
2021-05-01 10:59:04 +0200

    fixed onclick with viewBinding

--- app/src/main/java/com/example/play71/MyFragment.kt
+++ app/src/main/java/com/example/play71/MyFragment.kt
@@ -1,31 +1,62 @@
 package com.example.play71
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.play71.databinding.ActivityMainBinding
+import com.example.play71.databinding.FragmentMyBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.parcelize.Parcelize
 
 
 @AndroidEntryPoint
 class MyFragment : Fragment(R.layout.fragment_my) {
     private val myViewModel: MyViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentMyBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         println("MyDebug: ViewModel injected into fragment: $myViewModel")
+        binding = FragmentMyBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("MyDebug: ViewModel injected into fragment: $myViewModel")
+
+        binding = FragmentMyBinding.inflate(LayoutInflater.from(context))
 
         val myStateEvent : MyStateEvent.InsertBabyNameEvent =  MyStateEvent.InsertBabyNameEvent("Baby Jesus")
         println("MyDebug: myStateEvent: $myStateEvent")
 
-        println("MyDebug: Calling ViewModel SetStateEvent with the InsertBabyNameEvent")
-        myViewModel.setStateEvent(myStateEvent)
-    }
+        binding.button.setOnClickListener {
+            println("MyDebug: Calling ViewModel SetStateEvent with the InsertBabyNameEvent")
+            myViewModel.setStateEvent(myStateEvent)
+        }
+    }*/
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val myStateEvent : MyStateEvent.InsertBabyNameEvent =  MyStateEvent.InsertBabyNameEvent("Baby Jesus")
+        println("MyDebug: myStateEvent: $myStateEvent")
+
+        binding.button.setOnClickListener {
+            println("MyDebug: Calling ViewModel SetStateEvent with the InsertBabyNameEvent")
+            myViewModel.setStateEvent(myStateEvent)
+        }
+
         subscribeObservers()
     }
 
